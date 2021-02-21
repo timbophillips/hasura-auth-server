fb5429c983a5dace8034949f04c2240441baf3f6
timbophillips <timothy.john.phillips@gmail.com>
2021-02-21 15:41:16 +0800

    testing

---
+++ src/test-scripts/test-local-and-heroku.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+bash $(dirname "$0")/test-script.bash \
+    localhost:3000 \
+    Dad \
+    password \
+    https://smacking.hasura.app/v1/graphql \
+    "{ users { id username } }"
+
+bash $(dirname "$0")/test-script.bash \
+    https://smacking-hasura-auth.herokuapp.com \
+    Dad \
+    password \
+    https://smacking.hasura.app/v1/graphql \
+    "{ users { id username } }"
--- src/test-scripts/test-script-local.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bash $(dirname "$0")/test-script.bash localhost:3000 Dad password https://smacking.hasura.app/v1/graphql "{ users { id username } }"
